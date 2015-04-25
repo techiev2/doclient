60a73b093aa95e43b153a5c06ff7b7ca412177a4
Sriram Velamur <sriram.velamur@gmail.com>
2015-04-25 21:14:26 +0530

    Fixture for setting id from API response and id property
    1. Adds a base id property that would be used by derived classes. API
    responds with an id key used with init which is a restricted keyword. So
    a check in __init__'s setattr is added to set an incoming 'id' value as
    _id
    
    2. An id property is added overriding the base object identifier

--- doclient/base.py
+++ doclient/base.py
@@ -8,6 +8,15 @@ sys.dont_write_bytecode = True
 
 
 class BaseObject(object):
+
+    _id = None
+
     def __init__(self, **kwargs):
         for name, value in kwargs.iteritems():
+            if name == "id":
+                name = "_id"
             setattr(self, name, value)
+
+    @property
+    def id(self):
+        return getattr(self, "_id")
