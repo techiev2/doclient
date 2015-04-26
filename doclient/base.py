4d9068c68a9a1838325beddb2a63a2ef12c571bf
Sriram Velamur <sriram.velamur@gmail.com>
2015-04-26 14:47:32 +0530

    Linting fixes, as_dict/as_json methods and fix for id property
    1. Adds docstring for BaseObject class
    
    2. Adds as_dict and as_json methods to return dict/json representation
    of objects for use in shell.
    
    3. Adds a fix for the id property (a builtin override) by changing
    property to _id and overriding the __getattr__ method.

--- doclient/base.py
+++ doclient/base.py
@@ -5,18 +5,46 @@ __all__ = ("BaseObject",)
 
 import sys
 sys.dont_write_bytecode = True
+from json import dumps
 
 
 class BaseObject(object):
 
+    """
+    BaseObject class for use with doclient objects.
+    Provides base id property and init parameter setter.
+    """
+
     _id = None
+    props = []
 
     def __init__(self, **kwargs):
+        """BaseObject class init"""
         for name, value in kwargs.iteritems():
             if name == "id":
                 name = "_id"
             setattr(self, name, value)
+            self.props.append(name)
 
-    @property
-    def id(self):
-        return getattr(self, "_id")
+    def as_dict(self):
+        """Dictionary repr for BaseObject objects"""
+        return {k: getattr(self, k, None) for k in self.props}
+
+    def as_json(self):
+        """JSON repr method for BaseObject objects"""
+        data = self.as_dict()
+        _data = {}
+        for name, value in data.iteritems():
+            if isinstance(value, BaseObject):
+                value = value.as_json()
+            _data[name] = value
+        return dumps(_data)
+
+    def __getattr__(self, key):
+        """
+        Overridden __getattr__ method to work with id property
+        """
+        if key == "id":
+            return self._id
+        else:
+            return self.key
