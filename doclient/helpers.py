d40e96e44e360b46b4776d08ecf31336faf5334b
Sriram Velamur <sriram.velamur@gmail.com>
2015-04-29 01:14:35 +0530

    Adds helpers module
    Adds a helpers module with a set_caller decorator to set caller
    information into callee. Sets client as a class property in called
    classes' methods

---
+++ doclient/helpers.py
@@ -0,0 +1,21 @@
+#! coding=utf-8
+"""DigitalOcean APIv2 helpers module"""
+__author__ = "Sriram Velamur <sriram.velamur@gmail.com>"
+__all__ = ("set_caller",)
+
+import sys
+sys.dont_write_bytecode = True
+
+
+def set_caller(function):
+    """
+    Decorator to set reference DOClient object to callee object instance.
+    """
+    def wrapper(cls, *args, **kwargs):
+        """Wrapper method"""
+        cls.client = sys._getframe(1).f_locals.get("self")
+
+        return function(cls, *args, **kwargs)
+
+    return wrapper
+
