8dd831b468df1ecf7d343a50b1bb72ea9c3ff09f
Sriram Velamur <sriram.velamur@gmail.com>
2017-05-01 01:15:11 +0530

    Changes docstring to work with Sphinx

--- doclient/helpers.py
+++ doclient/helpers.py
@@ -8,11 +8,11 @@ sys.dont_write_bytecode = True
 
 
 def set_caller(function):
-    """
+    r"""
     Decorator to set reference DOClient object to callee object instance.
     """
     def wrapper(cls, *args, **kwargs):
-        """Wrapper method"""
+        r"""Wrapper method"""
         cls.client = getattr(sys, "_getframe")(1).f_locals.get("self")
 
         return function(cls, *args, **kwargs)
