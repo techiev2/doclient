72046d61158b5a2512e37d05b38920d58c2502d2
Sriram Velamur <sriram.velamur@gmail.com>
2017-04-30 14:27:24 +0530

    Adds setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='do-client',
+    version='1.0.6',
+    description='DigitalOcean REST API python client',
+    author='Sriram Velamur',
+    author_email='sriram.velamur@gmail.com',
+    url='https://github.com/techiev2/doclient',
+    packages=['doclient',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    install_requires=['requests','pyopenssl>=0.13','ndg-httpsclient','pyasn1']
+)
